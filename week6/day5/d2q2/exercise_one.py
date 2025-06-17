fb180b19e87b18f67cbd075401ad4ac6cbc63a63
Nirmal Rampersand <nirmal@Nirmals-MacBook.local>
2025-06-17 20:31:58 +0400

    new

---
+++ week6/day5/d2q2/exercise_one.py
@@ -0,0 +1,12 @@
+# from func import func #from file import function
+
+
+# result = func(5, 10)
+# print(f"The result of the function is: {result}")
+
+
+import func # Import the module func
+
+
+result = func.func(5, 10)
+print(f"The result of the function is: {result}")
